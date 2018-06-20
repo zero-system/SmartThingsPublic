b8df5f65f2d3d4e3a143aabd18d056eb2bef4163
Zero_System <zero@system.lc>
2018-06-20 14:28:16 -0600

    Added new user input section for forced temperature control.

--- smartapps/zero-system/pid-fan-controller-advanced.src/pid-fan-controller-advanced.groovy
+++ smartapps/zero-system/pid-fan-controller-advanced.src/pid-fan-controller-advanced.groovy
@@ -27,45 +27,67 @@ definition(
 // @formatter:off
 preferences
 {
-    section()
-    {
-        paragraph "Choose a temperature sensor. If multiple temp sensors are choosen, the average will be taken"
-        input( title: "Temperature Sensor(s)" , name: "tempSensors" , type: "capability.temperatureMeasurement" , multiple: true , required: true )
-
-        paragraph "Select the cooling fan controller (dimmer)"
-        input( title: "Fan Controller(s)" , name: "fans" , type: "capability.switchLevel" , multiple: true , required: true )
-
-        paragraph "Set the desired target temperature"
-        input( title: "Target Temp" , name: "targetTemp" , type: "decimal" , required: true , description: "70 (deg)" , defaultValue: 70 )
+	page( name: "settingsPID" , title: "PID Control Settings" , nextPage: "settingsForced" )
+	{
+	    section()
+	    {
+	        paragraph "Choose a temperature sensor. If multiple temp sensors are choosen, the average will be taken"
+	        input( title: "Temperature Sensor(s)" , name: "tempSensors" , type: "capability.temperatureMeasurement" , multiple: true , required: true )
 	
-	    paragraph "Set the minimum temperature. If the temperature reaches the set minimum, the fan will be turned off, overriding the minimum fan speed"
-	    input( title: "Minimum Temperature" , name: "minTemp" , type: "decimal" , required: true , description: "70 (deg)" , defaultValue: 65 )
-
-        paragraph "Set the minimum fan speed. This prevents the fan from turning on and off at low speeds"
-        input( title: "Minimum Fan Speed" , name: "minFanLevel" , type: "decimal" , required: true , description: "10%" , range: "0..100" , defaultValue: 10 )
-
-        paragraph "Sampling Time. It is the time between each measurement. Lower time means a faster rate of adjustment"
-        input( title: "Sampling Time" , name: "samplingTime" , type: "enum" , required: true , options: ["1-Minute" , "5-Minutes" , "10-Minutes" , "15-Minutes"] , defaultValue: "1-Minute" )
-        
-        paragraph "Reverse control direction. Enable to reverse the direction of control. For example, heating."
-        input( title: "Reverse Control Direction" , name: "reverseDirection" , type: "bool" , required: true , defaultValue: false )
-        
-        paragraph "Enable PID Control during time frame. Set time frame below. For example, 09:00 - 17:00"
-        input( title: "Enable Time Frame" , name: "enableTimeFrame" , type: "bool" , required: true , defaultValue: false )
-    }
-
-    section( "Time frame." , hideable: true , hidden: true )
-    {
-        input( title: "Start Time" , name: "startTime" , type: "time" , required: false , discription: "09:00" , defaultValue: "09:00" )
-        input( title: "Stop Time" , name: "stopTime" , type: "time" , required: false , discription: "17:00" , defaultValue: "17:00")
-    }
-
-    section( "Set PID variables." , hideable: true , hidden: true )
-    {
-        input( title: "P Variable" , name: "pVar" , type: "decimal" , required: false , description: "20" , defaultValue: 20 )
-        input( title: "I Variable" , name: "iVar" , type: "decimal" , required: false , description: "1" , defaultValue: 1 )
-        input( title: "D Variable" , name: "dVar" , type: "decimal" , required: false , description: "10" , defaultValue: 10 )
-    }
+	        paragraph "Select the cooling fan controller (dimmer)"
+	        input( title: "Fan Controller(s)" , name: "fans" , type: "capability.switchLevel" , multiple: true , required: true )
+	
+	        paragraph "Set the desired target temperature"
+	        input( title: "Target Temp" , name: "targetTemp" , type: "decimal" , required: true , description: "70 (deg)" , defaultValue: 70 )
+		
+		    paragraph "Set the minimum temperature. If the temperature reaches the set minimum, the fan will be turned off, overriding the minimum fan speed"
+		    input( title: "Minimum Temperature" , name: "minTemp" , type: "decimal" , required: true , description: "70 (deg)" , defaultValue: 65 )
+	
+	        paragraph "Set the minimum fan speed. This prevents the fan from turning on and off at low speeds"
+	        input( title: "Minimum Fan Speed" , name: "minFanLevel" , type: "decimal" , required: true , description: "10%" , range: "0..100" , defaultValue: 10 )
+	
+	        paragraph "Sampling Time. It is the time between each measurement. Lower time means a faster rate of adjustment"
+	        input( title: "Sampling Time" , name: "samplingTime" , type: "enum" , required: true , options: ["1-Minute" , "5-Minutes" , "10-Minutes" , "15-Minutes"] , defaultValue: "1-Minute" )
+	        
+	        paragraph "Reverse control direction. Enable to reverse the direction of control. For example, heating."
+	        input( title: "Reverse Control Direction" , name: "reverseDirection" , type: "bool" , required: true , defaultValue: false )
+	        
+	        paragraph "Enable PID Control during time frame. Set time frame below. For example, 09:00 - 17:00"
+	        input( title: "Enable Time Frame" , name: "enableTimeFrame" , type: "bool" , required: true , defaultValue: false )
+	    }
+	
+	    section( "Time frame." , hideable: true , hidden: true )
+	    {
+	        input( title: "Start Time" , name: "startTime" , type: "time" , required: $enableTimeFrame , discription: "09:00" , defaultValue: "09:00" )
+	        input( title: "Stop Time" , name: "stopTime" , type: "time" , required: $enableTimeFrame , discription: "17:00" , defaultValue: "17:00")
+	    }
+	
+	    section( "Set PID variables." , hideable: true , hidden: true )
+	    {
+	        input( title: "P Variable" , name: "pVar" , type: "decimal" , required: false , description: "20" , defaultValue: 20 )
+	        input( title: "I Variable" , name: "iVar" , type: "decimal" , required: false , description: "1" , defaultValue: 1 )
+	        input( title: "D Variable" , name: "dVar" , type: "decimal" , required: false , description: "10" , defaultValue: 10 )
+	    }
+	}
+	page( name: "settingsForced" , title: "Forced Temperature Control Settings" )
+	{
+		section( "Enable forced temperature control when outside \"PID Time Frame\"" )
+		{
+			paragraph "When enabled, the controller will turn on a switch that controls a forced temperature device. For example, a smart switch connected to an Air Conditioner."
+			input( title: "Enable Forced Temperature Control" , name: "enableForced" , type: "bool" , required: true , defaultValue: false )
+		}
+		
+		section( "Forced Cooling Temperature Control Settings" , hideable: true , hidden: true)
+		{
+			paragraph "Select the cooling device (switch), that Will cool room the room to Target Temperature: $targetTemp"
+			input( title: "Cooling Device" , name: "forcedCooling" , type: "capability.switch" , multiple: true , required: false )
+		
+			paragraph "Enable ON/OFF control? Some A/C have built in thermostats and just need to be powered on and not turned on/off"
+			input( title: "Enable ON/OFF Control" , name: "enableCoolingControl" , type: "bool" , required: false , defaultValue: false )
+		}
+		
+		
+	}
 }
 // @formatter:on
 
